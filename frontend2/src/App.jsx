20af660c77aee0a803b70d03d3038384ac17f86b
Vismaya-a <vismaya.a.m.2222@gmail.com>
2025-01-02 16:54:05 +0530

    first commit

--- frontend2/src/App.jsx
+++ frontend2/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './components/Home';
 import Register from './components/Register';
 import Login from './components/Login';
 import './index.css';
